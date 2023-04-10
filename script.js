4b31a03b86201a8dae042b003ba127056b7b6f53
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-10 23:34:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function fibonacci(num) {
 
-	if(num <= 1){
-		
-		return num;
+	if(num <= 2){
+		return num-1;
 	}
-	let n1 = 0;
-	let n2 =1;
-	var next = 0;
+	
+	let n1 =0; // 1
+	let n2 =1; // 2
+	var next = 0; // 2
 	var number = parseInt(num);
-	for(i =2;i <=number+1 ; i++){
+	for(i =3;i <=number ; i++){
 		next = n1+n2;
 		n1 = n2;
 		n2 = next;
